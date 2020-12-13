eff11fab74b2f52e8d63ea0942ab93e1ffae15fc
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2020-12-13 18:08:20 +0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -3,10 +3,22 @@
 #include <stdlib.h>
 void main()
 {
-    int i, r;
+    int i;
+    float q,r;
     srand(time(NULL));
     for (i = 0; i < 5; i++) {
         r = rand()%4;
-        printf("%d\n", r);
-    }
+        q = rand();
+        while(q>1){
+        q=q/10;
+        }
+        if (r==3){
+            printf("%f\n",r);
+        }
+        else{
+        q=q/10;
+        r=r+q;
+        printf("%f\n",r);
+        }   
+        }
 }
